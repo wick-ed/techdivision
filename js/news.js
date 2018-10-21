819f67a78468f946debcd503328f5408ded63b3f
Artur Faltenberg <artur.faltenberg@protonmail.com>
2018-10-21 10:31:41 +0200

    HTML skeleton ready for injection.

--- js/news.js
+++ js/news.js
@@ -1,15 +1,43 @@
-const NEWS_URL = "https://larsroettig.de/news.json";
-//const NEWS_URL = "file.txt";
+//const NEWS_URL = "https://larsroettig.de/news.json?jsoncallback=?";
+const NEWS_URL = "demo.json";
+const REFRESH_TIME = 10;  // seconds
+
+var news = [];
 
 
 function start() {
-  var req = $.ajax(NEWS_URL)
-      .done(function() {
-        alert("done");
-      })
-      .fail(function(data, status, error) {
-        alert("status: " + status + ", error: " + error);
-      });
+  var update = function() {
+    fetchNews();
+    window.setTimeout(displayNews, 1000);
+  }
 
-  document.getElementById("root").innerHTML = "Hello, world!";
+  update();
+  window.setInterval(update, REFRESH_TIME*1000);
+}
+
+
+function fetchNews() {
+  $.getJSON(NEWS_URL)
+    .done(function(data) {
+      news = [];
+      $.each(data, function(key, val) {
+        news.push(val);
+      });
+    })
+    .fail(function(data, status, error) {
+      alert("status: " + status + ", error: " + error);
+    });
+}
+
+
+function displayNews() {
+  var messages = [];
+  $.each(news, function(index, message) {
+    var items = [];
+    $.each(message, function(key, val) {
+      items.push("<li class=\"" + key + "\">" + val + "</li>");
+    });
+    messages.push("<ul>" + items.join("") + "</ul>");
+  });
+  $("#main").html(messages);
 }
